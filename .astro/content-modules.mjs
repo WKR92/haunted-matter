987c1d15bd6ef5b284d5b86dfdcc799c26e7c8bb
Wiktor <w.k.r.1992@gmail.com>
2025-05-22 23:40:55 +0200

    stoneAndLeaf adventure added

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,1 +1,6 @@
-export default new Map();
+
+export default new Map([
+["src/content/obsidian/home.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fobsidian%2Fhome.mdx&astroContentModuleFlag=true")],
+["src/content/obsidian/Rules.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fobsidian%2FRules.mdx&astroContentModuleFlag=true")],
+["src/content/obsidian/The boundary between stone and leaf.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fobsidian%2FThe+boundary+between+stone+and+leaf.mdx&astroContentModuleFlag=true")]]);
+		
