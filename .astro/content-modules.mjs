9044c92e1da3fb535e3fefffa7ead4b718b3d60d
Wiktor <w.k.r.1992@gmail.com>
2026-02-07 19:12:06 +0100

    magic changes

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,8 +1,8 @@
 
 export default new Map([
-["src/content/obsidian/Rules.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fobsidian%2FRules.mdx&astroContentModuleFlag=true")],
 ["src/content/obsidian/home.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fobsidian%2Fhome.mdx&astroContentModuleFlag=true")],
 ["src/content/obsidian/The boundary between stone and leaf.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fobsidian%2FThe+boundary+between+stone+and+leaf.mdx&astroContentModuleFlag=true")],
-["src/content/obsidian/The World of Zaya.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fobsidian%2FThe+World+of+Zaya.mdx&astroContentModuleFlag=true")],
-["src/content/obsidian/Witnesses of Yesterday.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fobsidian%2FWitnesses+of+Yesterday.mdx&astroContentModuleFlag=true")]]);
+["src/content/obsidian/Witnesses of Yesterday.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fobsidian%2FWitnesses+of+Yesterday.mdx&astroContentModuleFlag=true")],
+["src/content/obsidian/Rules.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fobsidian%2FRules.mdx&astroContentModuleFlag=true")],
+["src/content/obsidian/The World of Zaya.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fobsidian%2FThe+World+of+Zaya.mdx&astroContentModuleFlag=true")]]);
 		
